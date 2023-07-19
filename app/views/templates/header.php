dff996ff2291de2790c28bbb2d33e9af7537ab0e
Rangga Eka Kusuma Dani <ranggaeka165@gmail.com>
2023-07-19 18:43:29 +0700

    update home

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,7 +1,7 @@
 <?php
 // Cek apakah sesi sudah aktif sebelum memulai sesi
 if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+  session_start();
 }
 
 
@@ -10,49 +10,68 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Halaman <?= $data['judul']; ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
+  <meta charset="UTF-8">
+  <title>Halaman
+    <?= $data['judul']; ?>
+  </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
 
-    <style>
-      html,
-      body {
-        height: 100%;
-        margin: 0;
-      }
+  <style>
+    html,
+    body {
+      height: 100%;
+      margin: 0;
+    }
 
-      .content {
-        min-height: 100%;
-        margin-bottom: -60px;
-      }
-      
-      .image-preview{
-        max-height: 20px;
-      }
+    .content {
+      min-height: 100%;
+      margin-bottom: -60px;
+    }
 
-      .footer {
-        height: 60px; /* Tinggi footer */
-        background-color: #f5f5f5;
-        padding: 10px;
-      }
-      .bg-image-vertical {
-        position: relative;
-        overflow: hidden;
-        background-repeat: no-repeat;
-        background-position: right center;
-        background-size: auto 100%;
-        max-height: 200px
-      }
-    </style>
+    .image-preview {
+      max-height: 20px;
+    }
+
+    .footer {
+      height: 60px;
+      /* Tinggi footer */
+      background-color: #f5f5f5;
+      padding: 10px;
+    }
+
+    .bg-image-vertical {
+      position: relative;
+      overflow: hidden;
+      background-repeat: no-repeat;
+      background-position: right center;
+      background-size: auto 100%;
+      max-height: 200px
+    }
+
+    .card-img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+
+    .equal-image-size {
+      height: 300px;
+      /* Tentukan ukuran tinggi gambar yang diinginkan */
+    }
+  </style>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-      <a class="navbar-brand" href="<?= BASEURL; ?>">EventWise</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="<?= BASEURL; ?>">EFG</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -84,10 +103,10 @@ if (session_status() === PHP_SESSION_NONE) {
           </div>
         <?php endif; ?>
       </div>
-  </div>
-</nav>
+    </div>
+  </nav>
 
 
 
-<div class="content">
-<!-- Your remaining HTML code -->
+  <div class="content">
+    <!-- Your remaining HTML code -->
