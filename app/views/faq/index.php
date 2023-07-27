aac657df280155ff01a084ac33730126572152ec
Ridwan Nurhakim <ridwann690@gmail.com>
2023-07-27 19:45:52 +0700

    faq

---
+++ app/views/faq/index.php
@@ -0,0 +1,66 @@
+
+<header>
+    <h1>FAQ - Event Management</h1>
+</header>
+<div>
+    <h1>FAQ - Pertanyaan yang Sering Diajukan</h1>
+</div>
+
+
+<div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item">
+            <h2 class="according-header" id="flush-headingOne">
+                <buuton class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <?php echo $faq['question']; ?>
+                </button>
+            </h2>
+
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body"><?php echo $faq['answer']; ?></div>
+            </div>
+    </div>
+    <div class="accordion-item">
+            <h2 class="according-header" id="flush-headingTwo">
+                <buuton class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseTwo">
+                <?php echo $faq['question']; ?>
+                </button>
+            </h2>
+
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body"><?php echo $faq['answer']; ?></div>
+            </div>
+    </div>
+    <div class="accordion-item">
+            <h2 class="according-header" id="flush-headingThree">
+                <buuton class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseThree">
+                <?php echo $faq['question']; ?>
+                </button>
+            </h2>
+
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body"><?php echo $faq['answer']; ?></div>
+            </div>
+    </div>
+    <div class="accordion-item">
+            <h2 class="according-header" id="flush-headingFour">
+                <buuton class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseFour">
+                <?php echo $faq['question']; ?>
+                </button>
+            </h2>
+
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body"><?php echo $faq['answer']; ?></div>
+            </div>
+    </div>
+    <div class="accordion-item">
+            <h2 class="according-header" id="flush-headingFive">
+                <buuton class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseFive">
+                <?php echo $faq['question']; ?>
+                </button>
+            </h2>
+
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body"><?php echo $faq['answer']; ?></div>
+            </div>
+    </div>
+</div>
